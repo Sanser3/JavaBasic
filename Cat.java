ea56887186ae8ac35cbccead15998c4ed50204f6
Sanser3 <98323544+Sanser3@users.noreply.github.com>
2022-10-12 16:12:14 +0200

    Delete Cat.java

--- Cat.java
+++
@@ -1,9 +0,0 @@
-public class Cat extends Animal implements IAnimal {
-    public Cat(String name, String color, int age) {
-        super(name, color, age);
-    }
-
-    public String voice() {
-        return "meow";
-    }
-}
